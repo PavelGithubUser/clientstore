671eae6a2709b9be9fe475814f440ccc5fc0e57d
Pavel <Pavel.WebOnline@gmail.com>
2019-03-22 12:32:52 +0300

    Added orders table to view
    It need for displayed and editing orders;

--- src/app/tabs/orders/orders.component.ts
+++ src/app/tabs/orders/orders.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {OrdersService} from '../../services/orders.service';
+import {OrderModel} from '../../model/order.model';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ordersService: OrdersService) {}
+
+  orderModels: OrderModel[];
+  responseStatus: number;
 
   ngOnInit() {
+    this.initOrderModels();
+  }
+
+  initOrderModels() {
+    this.ordersService.getAllOrders().subscribe(orders => {
+      this.orderModels = orders;
+    });
+  }
+
+  deleteOrder(idOrder: number) {
+    const indexOrder = this.orderModels.findIndex(orderModel => orderModel.id === idOrder);
+    this.ordersService.deleteOrderById(idOrder).subscribe(response => {
+      if (response instanceof HttpResponse) {
+        this.responseStatus = response.status;
+        this.orderModels.splice(this.orderModels.findIndex(orderModels => orderModels.id === indexOrder), 1);
+      }
+    }, error => {
+      if (error instanceof HttpErrorResponse) {
+        this.responseStatus = error.status;
+      }
+    });
   }
 
 }
